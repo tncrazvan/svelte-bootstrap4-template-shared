da173f2ee51cec25993b24edd93cf5f92814158a
Razvan <tangent.jotey@gmail.com>
2020-05-15 05:21:06 +0200

    Tooltip arrows now behave correctly accodring to the specified position attribute

--- component/global/Tooltip.svelte
+++ component/global/Tooltip.svelte
@@ -4,11 +4,13 @@
 	export let position = "top";
 	export let target;
 	let x=0,y=0;
-	let tooltip;
 	let show = false;
+	let deltaForArrowX = 0;
+	let arrowWrapper;
 	const defaultStyle = "max-width: 300px;max-height: 200px;";
 	let customStyle = ""; export {customStyle as style};
 	let forceEndPoll = false;
+	let arrowEl;
 	onMount(async ()=>{
 		(function poll(){
 			if(target){
@@ -17,7 +19,7 @@
 				return;
 			}
 			if(forceEndPoll) return;
-			setTimeout(poll,100);
+			setTimeout(poll,200);
 		})();
 	});
 	
@@ -33,32 +35,79 @@
 		show = false;
 	}
 	
-	function manage(tooltip){
+	function manageArrow(arrow){
+		arrowEl = arrow;
+		switch(position){
+			case "top":
+				arrow.style.left = "auto";
+				arrow.style.right = "auto";
+				arrow.style.top = "auto";
+				arrow.style.bottom = "-0.4rem";
+				arrow.style.transform = "rotateZ(-135deg)";
+				break;
+			case "bottom":
+				arrow.style.left = "auto";
+				arrow.style.right = "auto";
+				arrow.style.top = "-0.4rem";
+				arrow.style.bottom = "auto";
+				arrow.style.transform = "rotateZ(45deg)";
+				break;
+			case "left":
+				arrow.style.left = "auto";
+				arrow.style.right = "-0.4rem";
+				arrow.style.top = "auto";
+				arrow.style.bottom = "auto";
+				arrow.style.transform = "rotateZ(130deg)";
+				break;
+			case "right":
+				arrow.style.left = "-0.4rem";
+				arrow.style.right = "auto";
+				arrow.style.top = "auto";
+				arrow.style.bottom = "auto";
+				arrow.style.transform = "rotateZ(-45deg)";
+				break;
+		}
+	}
+
+	function manageTooltip(tooltip){
 		const rect = target.getBoundingClientRect();
 		switch(position){
 			case "top":
 				x = rect.x + rect.width/2 - tooltip.offsetWidth/2;
 				y = rect.y - tooltip.offsetHeight - 10; //POSITION
-				if(y < 0)
+				if(y < 0){
 					y = rect.y + rect.height - 10; //revert to preferred "bottom" POSITION
+					position = "bottom";
+					if(arrowEl) manageArrow(arrowEl);
+				}
 
 				//make sure x is within bounds
-				if(x < 0) x = 0;
-				if(x + tooltip.offsetWidth > window.main.offsetWidth)
+				if(x < 0){
+					deltaForArrowX = x;
+					x = 0;
+				}
+				if(x + tooltip.offsetWidth > window.main.offsetWidth){
 					x = window.main.offsetWidth - tooltip.offsetWidth;
+				}
 				break;
 			case "bottom":
 				x = rect.x + rect.width/2 - tooltip.offsetWidth/2;
 				y = rect.y + rect.height + 10; //POSITION
-				if(y + tooltip.offsetHeight >= window.main.offsetHeight)
+				if(y + tooltip.offsetHeight >= window.main.offsetHeight){
 					y = rect.y - tooltip.offsetHeight + 10; //revert to preferred "top" POSITION
+					position = "top";
+					if(arrowEl) manageArrow(arrowEl);
+				}
 
 				//make sure x is within bounds
-				if(x < 0) x = 0;
-				if(x + tooltip.offsetWidth > window.main.offsetWidth)
+				if(x < 0){
+					deltaForArrowX = x;
+					x = 0;
+				}
+				if(x + tooltip.offsetWidth > window.main.offsetWidth){
 					x = window.main.offsetWidth - tooltip.offsetWidth;
+				}
 				break;
-
 			case "left":
 				y = rect.y + rect.height / 2 - tooltip.offsetHeight / 2; //center the tooltip
 				x = rect.x - tooltip.offsetWidth - 10; //move it to the left side
@@ -94,10 +143,10 @@
 	}
 </script>
 {#if show}
-<div use:manage class="card" transition:fly={getTransition()} style="left:{x}px;top:{y}px;{defaultStyle}{customStyle}">
+<div use:manageTooltip class="card" transition:fly={getTransition()} style="left:{x}px;top:{y}px;{defaultStyle}{customStyle}">
 	<div class="card-body">
 		<div class="arrow-wrapper">
-			<div class="arrow"></div>
+			<div use:manageArrow style="margin-left:{deltaForArrowX*2}px" class="arrow"></div>
 		</div>
 		<slot></slot>
 	</div>
@@ -119,21 +168,22 @@
 	}
 	.arrow-wrapper{
 		position: absolute;
-		top: -0.4rem;
+		top: 0;
+		bottom: 0;
 		left: 0;
 		right: 0;
 		display: grid;
 		grid-template-columns: 1fr;
 		grid-template-rows: 1fr;
 		justify-items: center;
+		align-items: center;
 	}
 	.arrow{
-		position: relative;
+		position: absolute;
 		background: #ffffff;
 		border-left: 1px solid #e3e3e3;
 		border-top: 1px solid #e3e3e3;
 		width: 1rem;
 		height: 1rem;
-		transform: rotateZ(45deg)
 	}
 </style>
